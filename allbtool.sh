573ba25187d0dbf866f36b4248aa7cfa2fbde58c
rootslayer <john.ciavarella@gmail.com>
2017-02-09 22:15:02 -0500

    Removed garbage btool vars

--- allbtool.sh
+++ allbtool.sh
@@ -6,8 +6,8 @@
 ##  2017-02-09                         ##
 #########################################
 #### Varibables
+declare -a modules=("alert_actions" "app" "audit" "authentication" "authorize" "checklist" "collections" "commands" "crawl" "datamodels" "deploymentclient" "distsearch" "event_renderers" "eventtypes" "fields" "indexes" "inputs" "instance.cfg" "limits" "literals" "macros" "multikv" "outputs" "passwords" "procmon-filters" "props" "pubsub" "restmap" "savedsearches" "searchbnf" "segmenters" "server" "serverclass" "source-classifier" "sourcetypes" "tags" "telemetry" "times" "transactiontypes" "transforms" "ui-prefs" "user-seed" "visualizations" "viewstates" "web" "wmi" "workflow_actions")
 hostName=`hostname`
-declare -a modules=("alert_actions" "app" "audit" "authentication" "authorize" "checklist" "collections" "commands" "crawl" "datamodels" "default.meta" "deploymentclient" "distsearch" "event_renderers" "eventtypes" "fields" "indexes" "inputs" "instance.cfg" "limits" "literals" "macros" "multikv" "outputs" "passwords" "procmon-filters" "props" "pubsub" "restmap" "savedsearches" "searchbnf" "segmenters" "server" "serverclass" "serverclass.seed.xml" "source-classifier" "sourcetypes" "tags" "telemetry" "times" "transactiontypes" "transforms" "ui-prefs" "user-seed" "visualizations" "viewstates" "web" "wmi" "workflow_actions")
 date=`date +%s`
 date2=`date`
 
